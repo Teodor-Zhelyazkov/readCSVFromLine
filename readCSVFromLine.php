45b4b454020ef800a3bd8b427d2556c18bf2a559
Teodor-Zhelyazkov <teodor.zhelyazkov88@gmail.com>
2018-01-11 10:49:51 +0200

    Create readCSVFromLine.php

---
+++ readCSVFromLine.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+*   @method readCSVFromLine
+*   @param String
+*   @param Int
+*   @param Int
+*
+*   @return Array
+*/
+private function readCSVFromLine( $csvFile, $fromLine = 0, $linesToShow = 0 )
+{
+    $csvArray    = [];
+    $file_handle = fopen($csvFile, 'r');
+    $line        = 0;
+    $fileLines   = $this->fileLines;
+    $show        = 0;
+
+    if( $linesToShow != 0 || $linesToShow != "0")
+        $fileLines = $linesToShow;
+
+    while (!feof($file_handle) )
+    {
+        $row = fgetcsv($file_handle, 1024, ',');
+
+        if( $line >= $fromLine && $show < $fileLines)
+        {
+            if( !empty($row) )
+                $csvArray[] = $row;
+
+            $show ++;
+        }
+
+        $line ++;
+    }
+
+    fclose($file_handle);
+    return $csvArray;
+}
+
+?>
